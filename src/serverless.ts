a74126af9e77d0c261d058b84bcb11077fcf4fec
Flynn <wphestiraid@gmail.com>
2019-09-25 22:02:32 +0900

    Serverless 개발 환경 설정

---
+++ src/serverless.ts
@@ -0,0 +1,34 @@
+import serverless from "serverless-http";
+import getServer from "./app";
+import { APIGatewayProxyEvent, Context } from "aws-lambda";
+
+declare namespace ServerlessHttp {
+  type FrameworkApplication = {
+    callback: Function;
+    handle: Function;
+    router: {
+      route: Function;
+    };
+    _core: {
+      _dispatch: Function;
+    };
+  };
+  type HandlerCompatibleApp = Function | Partial<FrameworkApplication>;
+  type LambdaPartial = (
+    event: AWSLambda.APIGatewayProxyEvent,
+    context: AWSLambda.Context
+  ) => AWSLambda.APIGatewayProxyResult;
+}
+
+let serverlessApp: ServerlessHttp.LambdaPartial;
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+) => {
+  if (!serverlessApp) {
+    const app = await getServer();
+    serverlessApp = serverless(app);
+  }
+  return await serverlessApp(event, context);
+};
