adcbbf28448b64c0c24a6e016f21a8e30769c8d6
Flynn <wphestiraid@gmail.com>
2019-10-21 20:42:52 +0900

    User 모델 추가 및 커넥션 동작하도록 수정

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import { createConnection } from 'typeorm';
+import connectionOptions from './ormConfig';
 
 const getServer = async () => {
+  await createConnection(connectionOptions);
+
   const app = express();
 
   app.get('/', (req, res) => res.status(200).json({ success: true }));
