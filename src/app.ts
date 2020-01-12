510cf908d2f7b907ba972f3a96a8911791a1cc60
Flynn <wphestiraid@gmail.com>
2020-01-12 23:08:42 +0900

    app에 passport 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ dotenv.config();
 import express from 'express';
 import { createConnection } from 'typeorm';
 import { ApolloServer } from 'apollo-server-express';
+import { passportAuthenticate } from './utils/auth';
 import connectionOptions from './ormConfig';
 import schema from './schema';
+import './utils/auth';
 
 interface GetServiceResult {
   app: express.Application;
@@ -20,6 +22,8 @@ const getServer = async (): Promise<GetServiceResult> => {
     playground: true
   });
   const app = express();
+
+  app.use(passportAuthenticate);
 
   app.get('/status', (_, res) => res.status(200).json({ success: true }));
   server.applyMiddleware({ app });
